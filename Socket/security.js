b82f4129a2535f8a7c5368d9aa8c3029cdf15fa9
iTrooz_ <team.mortelgames@gmail.com>
2020-08-28 17:54:24 +0200

    finalisation

--- Socket/security.js
+++ Socket/security.js
@@ -18,17 +18,13 @@ function signMsg(msg, hexSalt){
 }
 
 function verifyMsg(msg, signature){
-    console.log("--")
-    console.log("MSG="+msg)
-    console.log("SIGNATURE="+signature)
     let list = signature.split(";")
     return signature==signMsg(msg, list[1])
 }
 
 
-// let msg = "log paper2"
-// let signature = signMsg(msg)
-// if(verifyMsg(msg, "Nbdf3X2qZb271kvHjBR/JKjN/rFS6hOyIZ+XoVDXvow=;60BA7BD825698697865C06FD940253E7")){
+// let msg = "log BungeeCord"
+// if(verifyMsg(msg, signature)){
 //     console.log("vérifié")
 // }else console.log("invalidé")
 
